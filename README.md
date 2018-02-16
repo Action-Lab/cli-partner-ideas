7f6fa77189dcb7f6f07682b64145d4c1eafc2cd8
Zorawar Moolenaar <zorawar87@gmail.com>
2018-02-16 15:24:58 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# cli-partner-ideas
+# cli-phc-ideas
+Display Google Forms application data on Public Humanities Collaborative web page
+
+## Live example
+http://commons.trincoll.edu/cli/phc-ideas (to be verified)
+
+## Requirements
+- Google Form responses must appear in Google Sheets, which must be published. Insert URLs in main.js
+- manually insert "Display" column in "Partners" Google Sheet, and manually insert "y" to approve display
+- embed iframes for Google Form and also for this GH-published repo in the web pages
+
+## Credits
+Created by Ilya Ilyankou (@ilyankou) for Liberal Arts Action Lab, Hartford CT
+Updated on 2018.02.16 by Zorawar Moolenaar (@zorawar87)
+
+## Dependencies
+* Tabletop v1.5.2, https://github.com/jsoma/tabletop
+* jQuery v3.2.1, https://jquery.com/
---
+++ main.js
@@ -0,0 +1,72 @@
+var ideasURL = 'https://docs.google.com/spreadsheets/d/10h3PmOuRoY9fJcpFoa1VJ0fSNrjYBFo0bc01FEpG_4s/pubhtml'
+
+Tabletop.init({
+  key: ideasURL,
+  callback: processData,
+  simpleSheet: true,
+});
+
+function mergeOrganizationAndWebsite(org, website) {
+  if (!website) { return org; }
+  return '<span class="invisible">' + org + '</span>'
+    + '<a target="_blank" href="http://' + website + '">' + org + '</a>';
+}
+
+function mergeTitleResearchNeeds(title, research, needs) {
+  res = '';
+  if (title) {
+    res += '<h3>' + title + '</h3>';
+  }
+
+  res += '<p><span class="emphasis">Public Humanities Project: </span>' + research + '</p>';
+
+  if (needs) {
+    res += '<p><span class="emphasis">Student Researchers: </span>' + needs + '</p>';
+  }
+
+  return res;
+}
+
+
+function emailToLink(name, email) {
+  if (!email) { return name; }
+  return name + ' <a href="mailto:' + email + '"> <i class="fa fa-envelope"> </a>';
+}
+
+
+function processData(data, tabletop) {
+  if (!data[0]) return;
+
+  var processedData = [];
+
+  for (i in data) {
+    var r = data[i];
+    if (r.Display !== 'y') continue;
+
+    // Add a row to the final dataset
+    processedData.push([
+      mergeOrganizationAndWebsite(r.Organization, r.Website) + '<br><br>' + emailToLink(r.Name, r.Email),
+      mergeTitleResearchNeeds(r.Title, r.Project, r['Student Researchers']),
+    ]);
+  }
+
+
+  $(document).ready(function() {
+    var table = $('#results').DataTable({
+      paging: false,
+      info: false,
+      ordering: true,
+      data: processedData,
+      columns: [
+        {title: 'Organization', width: '200px', className: 'td-center'},
+        {title: 'Project', orderable: false},
+      ]
+    });
+
+    $('input[name="filter"]').change(function() {
+      table.draw();
+    });
+
+  });
+
+}
